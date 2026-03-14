6765e68c01bbc7f8983d58472830428d3b5e2214
navatejavarma24 <navatejavarma24@gmail.com>
2026-03-14 11:11:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-<script>
 
-const toggleBtn = document.getElementById("themeToggle");
+const toggleButton = document.getElementById('theme-toggle-button');
 
-toggleBtn.addEventListener("click", function(){
-document.body.classList.toggle("light");
+
+
+
+toggleButton.addEventListener('click', function() {
+  
+  document.body.classList.toggle('dark-mode');
+  
+  
+  if (document.body.classList.contains('dark-mode')) {
+    toggleButton.textContent = 'Toggle light mode';
+    toggleButton.setAttribute('aria-label', 'Toggle light mode');
+  } else {
+    toggleButton.textContent = 'Toggle dark mode';
+    toggleButton.setAttribute('aria-label', 'Toggle dark mode');
+  }
 });
 
-</script>
