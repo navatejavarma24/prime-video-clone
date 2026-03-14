26690ff03edf9486b65042061b58a1738d4949f6
navatejavarma24 <navatejavarma24@gmail.com>
2026-03-14 11:19:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 
+const toggle = document.getElementById("toggleTheme");
+
+if(toggle){
+toggle.addEventListener("click",function(){
+document.body.classList.toggle("dark-mode");
+});
+}
+
+const form = document.getElementById("feedbackForm");
+
+if(form){
+
+form.addEventListener("submit",function(e){
+
+const name=document.getElementById("name").value;
+const email=document.getElementById("email").value;
+const message=document.getElementById("message").value;
+const error=document.getElementById("error");
+
+error.textContent="";
+
+if(name=="" || email=="" || message==""){
+e.preventDefault();
+error.textContent="All fields are required";
+}
+
+});
+}
